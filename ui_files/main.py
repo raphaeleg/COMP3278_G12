78778d665aef786e1d0eaa33ac7b7114ef5c3827
Win Chan <78186364+wincsw@users.noreply.github.com>
2022-10-22 20:51:05 +0800

    Update main.py

--- ui_files/main.py
+++ ui_files/main.py
@@ -3,13 +3,50 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("Login.ui", self)
+        username = self.usernameInput_lineEdit_login.text()
+        self.login_button_login.clicked.connect(self.login)
+    
+    def login(self):
+        # MISSING: check if username in database
+        #if (username in data base):
+        # loginface = LoginFace()
+        # widget.addWidget(loginface)
+        # widget.setCurrentIndex(widget.currentIndex() + 1)
+        #else:
+        #    self.lineEdit.clear
+
+        # TEMP: set to go to mainpage for simplicity
+        mainpage = MainPage()
+        widget.addWidget(mainpage)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class LoginFace(QDialog):
+    def __init__(self):
+        super(LoginFace, self).__init__()
+        loadUi("LoginFace.ui", self)
+        # MISSING: face detection code
+
+
 class MainPage(QDialog):
     def __init__(self):
         super(MainPage, self).__init__()
+        # MISSING: conditon for loading main page
+        #if (1 hr before class):
         loadUi("CourseInfo.ui", self)
+        #self.email_button_courseinfo.clicked.connect(self.emailMe)
+        #else:
+        #   loadUi("Timetable.ui", self)
+        
         self.login_history_button_courseinfo.clicked.connect(self.gotoLoginHistory)
         self.all_announcement_button_courseinfo.clicked.connect(self.gotoAnnouncement)
         self.logout_button_courseinfo.clicked.connect(self.exiting)
+
     
     def gotoAnnouncement(self):
         announcement = Announcement()
@@ -25,6 +62,9 @@ class MainPage(QDialog):
         print("Exiting")
         sys.exit(app.exec_())
         
+    # MISSING: email course info to student email
+    #def emailMe(self):
+        
 
 
 class LoginHistory(QDialog):
@@ -35,7 +75,7 @@ class LoginHistory(QDialog):
         self.logout_button_loginhistory.clicked.connect(self.exiting)
 
     def gotoMainPage(self):
-        mainpage = MainWindow()
+        mainpage = MainPage()
         widget.addWidget(mainpage)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -53,7 +93,7 @@ class Announcement(QDialog):
         self.logout_button_announcement.clicked.connect(self.exiting)
     
     def gotoMainPage(self):
-        mainpage = MainWindow()
+        mainpage = MainPage()
         widget.addWidget(mainpage)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -70,8 +110,8 @@ class Announcement(QDialog):
 # main
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
-mainpage = MainPage()
-widget.addWidget(mainpage)
+login = Login()
+widget.addWidget(login)
 widget.setFixedHeight(768)
 widget.setFixedWidth(1024)
 widget.show()
